2d462fc7cec34eafeb5aabf4c16f69754b468eb9
Peter & Frank Johnson <student@epicodus.com>
2015-04-23 18:20:31 -0700

    Cleaned up file for later work.

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -1,23 +1,17 @@
 class Fixnum
 
-  describe_method(:allergies) do
+  define_method(:allergies) do
 
-    allergens = { 1 => "eggs", 2 => "peanuts", 4 => "shellfish", 8 => "strawberries", 16 => "tomatoes", 32 => "chocolate", 64 => "pollen", 128 => "cats" }
-
-    #get self
-    #compare to keys from beginning
-    #stop at exceeding key
-    #begin subtracting lesser keys
-
-    score = []
-    self = query
-
-
-    allergens.each do |key, value|
-      
-    end
-
-    score
+    allergens = {
+      128 => "cats",
+      64 => "pollen",
+      32 => "chocolate",
+      16 => "tomatoes",
+      8 => "strawberries",
+      4 => "shellfish",
+      2 => "peanuts",
+      1 => "eggs"
+      }
 
   end
 end
--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -1,9 +1,9 @@
 require('rspec')
 require('allergies')
+require('pry')
 
   describe('Fixnum#allergies') do
     it('takes a number and tells you your allergies') do
       expect((3).allergies()).to(eq(["eggs", "peanuts"]))
     end
   end
-  
